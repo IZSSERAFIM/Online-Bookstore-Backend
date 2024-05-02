b91763c3a6b1ac111b76c66d095f8eb4dc5b220e
liruobin <king.alex.realmadrid@gmail.com>
2024-05-03 02:38:58 +0800

    finish hw7

--- src/main/java/org/onlinebookstore/onlinebookstorebackend/controllers/CartController.java
+++ src/main/java/org/onlinebookstore/onlinebookstorebackend/controllers/CartController.java
@@ -1,9 +1,9 @@
 package org.onlinebookstore.onlinebookstorebackend.controllers;
 
 import org.onlinebookstore.onlinebookstorebackend.entity.Cart;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import com.alibaba.fastjson2.JSONObject;
 
 import java.util.*;
@@ -22,6 +22,14 @@ public class CartController {
                 new Cart(3, "《编程珠玑》", 4900, "../imgs/book3.jpg", 1),
                 new Cart(4, "《代码大全》", 8800, "../imgs/book4.jpg", 1)
         );
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addCartItem(@RequestBody Map<String, String> payload) {
+        String bookId = payload.get("bookId");
+        // 在这里处理订单逻辑，例如添加到数据库等
+        System.out.println("Received order for bookID: " + bookId);
+        return ResponseEntity.ok("订单确认");
     }
 
 }
