ab5fb2f849acc1a775f2250e4bdc4327e400100d
liruobin <king.alex.realmadrid@gmail.com>
2024-05-16 03:21:22 +0800

    0516 finish profile->DB

--- src/main/java/org/onlinebookstore/onlinebookstorebackend/controllers/UserController.java
+++ src/main/java/org/onlinebookstore/onlinebookstorebackend/controllers/UserController.java
@@ -12,13 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/users")
 public class UserController {
     @Autowired
     UserService userService;
 
     @CrossOrigin
-//    @RequestMapping("/profile")
     @RequestMapping("/user")
     public ResponseEntity<User> getUserProfileHandler(@RequestBody UserDTO userDTO){
         return new ResponseEntity<>(userService.getProfile(userDTO), HttpStatus.ACCEPTED);
